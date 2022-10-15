63254714862cd4e32c323bd39d7b931b42b830ab
555 <fetisovden@yandex.ru>
2022-10-15 17:18:23 +0300

    1st commit

---
+++ src/main/java/pro/sky/SpringMockito_2_13/SpringMockito_2_13Application.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringMockito_2_13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMockito_2_13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMockito_2_13Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/SpringMockito_2_13/SpringMockito213ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.SpringMockito_2_13;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMockito213ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
